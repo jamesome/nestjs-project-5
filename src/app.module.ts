21e133225887a4ed9214e11ee73c3ef48873008d
jamesome <jamesome@naver.com>
2024-05-03 21:06:11 +0900

    설정중

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatModule } from './cat/cat.module';
 
 @Module({
-  imports: [CatModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
