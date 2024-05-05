e355df2908d94d8edacb585667997e69361754ad
jamesome <jamesome@naver.com>
2024-05-05 12:19:02 +0900

    HMR, 스웨거 추가

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('WMS Application')
+    .setDescription('The WMS API description')
+    .setVersion('1.0')
+    .addTag('wms')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+
+  await app.listen(3000);
 }
 bootstrap();
