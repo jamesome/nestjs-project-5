127b7def4e2fbcbfcb732c9078a05787a738bcc9
jamesome <jamesome@naver.com>
2024-05-13 09:30:05 +0900

    dataSource 환경변수 고정

--- dataSource.ts
+++ dataSource.ts
@@ -1,14 +1,13 @@
 import { ConfigService } from '@nestjs/config';
 import { DataSource } from 'typeorm';
-import { config } from 'dotenv';
+import { config as dotenvConfig } from 'dotenv';
 
 // package.json에 작성 된 typeorm-extension이 appConfigValidationSchema.module을 읽지 못하기 때문에 최상단에 작성 추가.
-config({
-  path: `${__dirname}/.env.${process.env.NODE_ENV}`,
-});
+// dotenvConfig({ path: `.env.${process.env.NODE_ENV}` });
+dotenvConfig({ path: `.env.development` });
 
 const configService = new ConfigService();
-console.log(__dirname);
+console.log(`.env.${process.env.NODE_ENV}`);
 export default new DataSource({
   type: 'mysql',
   host: configService.getOrThrow<string>('TENANT_DB_HOST'),
