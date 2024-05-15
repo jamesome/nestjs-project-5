6963353da3c93280569271b1f78857597be83051
jamesome <jamesome@naver.com>
2024-05-15 17:50:31 +0900

    Entity 공통 사항 분리

---
+++ src/modules/base-entity.ts
@@ -0,0 +1,25 @@
+import {
+  BaseEntity as TypeORMBaseEntity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+export abstract class BaseEntity extends TypeORMBaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  created_at!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updated_at?: Date | null = null;
+
+  @DeleteDateColumn({ name: 'deleted_at', nullable: true })
+  deleted_at?: Date | null = null;
+}
