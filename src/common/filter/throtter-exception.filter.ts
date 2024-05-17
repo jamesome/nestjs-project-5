bcf30c7afffeb255df7cb36c84f5871a0d748b21
jamesome <jamesome@naver.com>
2024-05-17 14:05:32 +0900

    쓰로틀러 수정

--- src/common/filter/throtter-exception.filter.ts
+++ src/common/filter/throtter-exception.filter.ts
@@ -3,23 +3,14 @@ import { ThrottlerException } from '@nestjs/throttler';
 
 @Catch()
 export class ThrottlerExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {
+  catch(exception: ThrottlerException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
-
-    if (!(exception instanceof ThrottlerException)) {
-      // ThrottlerException은 재 throw하여 처리하지 않고 그대로 통과시킴
-      throw exception;
-    }
-
     const status = exception.getStatus();
 
     response.status(status).json({
       statusCode: status,
-      message:
-        exception instanceof ThrottlerException
-          ? exception.message
-          : 'Internal Server Error',
+      message: exception.message,
     });
   }
 }
