3e9fac84525690a20cde23f8103410c920d300df
jamesome <jamesome@naver.com>
2024-05-18 22:59:17 +0900

    모듈 추가

--- src/modules/tenant/product/v1/product-v1.module.ts
+++ src/modules/tenant/product/v1/product-v1.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { ProductV1Repository } from './productV1.repository';
+import { UniqueProductNameValidator } from 'src/validators/unique-product-name.validator';
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService, ProductV1Repository],
+  providers: [ProductService, ProductV1Repository, UniqueProductNameValidator],
 })
 export class ProductV1Module {}
