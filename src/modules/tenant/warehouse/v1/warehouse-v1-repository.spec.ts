c3693fab878cd6d777d2ec8114ce25ca8df16e52
jamesome <jamesome@naver.com>
2024-05-20 21:32:34 +0900

    warehouse repository 제거

--- src/modules/tenant/warehouse/v1/warehouse-v1-repository.spec.ts
+++
@@ -1,7 +0,0 @@
-import { WarehouseV1Repository } from './warehouse-v1-repository';
-
-describe('WarehouseV1Repository', () => {
-  it('should be defined', () => {
-    expect(new WarehouseV1Repository()).toBeDefined();
-  });
-});
