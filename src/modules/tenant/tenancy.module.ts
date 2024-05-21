d93ffac4b976703af8b09ea89fc9520855aa96a9
jamesome <jamesome@naver.com>
2024-05-21 09:03:06 +0900

    멀티 테넌시 구성

--- src/modules/tenant/tenancy.module.ts
+++ src/modules/tenant/tenancy.module.ts
@@ -1,40 +1,35 @@
 import { Global, Module, Scope } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
-import { DataSource } from 'typeorm';
-import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { Request } from 'express';
 import { ConfigService } from '@nestjs/config';
-// import { WarehouseV1 } from './warehouse/v1/entities/warehouse-v1.entity';
 
 const connectionFactory = {
   provide: 'CONNECTION',
   scope: Scope.REQUEST,
   useFactory: async (request: Request) => {
-    // console.log(request);
-    const database = request.domain;
     const configService = new ConfigService();
+    const database = request.domain;
 
-    if (database) {
-      const typeOrmConfig = {
-        type: 'mysql',
-        host: configService.get<string>('TENANT_DB_HOST'),
-        port: configService.get<number>('TENANT_DB_PORT'),
-        username: configService.get<string>('TENANT_DB_USERNAME'),
-        password: configService.get<string>('TENANT_DB_PASSWORD'),
-        retryAttempts: 2, // DB connection 시도 횟수
-        logging: configService.get<string>('NODE_ENV') === 'development',
-        entities: ['dist/modules/**/**/**/entities/*.entity{.ts,.js}'],
-        // entities: [WarehouseV1],
-        autoLoadEntities: false, // 스키마 동기화. 테이블을 Drop 하고 Create 하는 방식. 데이터 보존 불가(develop환경에서만 사용!)
-      };
-
-      return new DataSource({
-        ...(typeOrmConfig as MysqlConnectionOptions),
-        database,
-      });
+    if (!database) {
+      return null;
     }
 
-    return null;
+    // MysqlConnectionCredentialsOptions
+    const dataSourceOptions: DataSourceOptions = {
+      type: 'mysql',
+      host: configService.get<string>('TENANT_DB_HOST'),
+      port: configService.get<number>('TENANT_DB_PORT'),
+      username: configService.get<string>('TENANT_DB_USERNAME'),
+      password: configService.get<string>('TENANT_DB_PASSWORD'),
+      database,
+      synchronize: false,
+      logging: configService.get<string>('NODE_ENV') === 'development',
+      entities: ['dist/modules/**/**/**/entities/*.entity{.ts,.js}'],
+    };
+
+    const dataSource = new DataSource(dataSourceOptions);
+    return await dataSource.initialize();
   },
   inject: [REQUEST],
 };
