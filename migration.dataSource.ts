3afd4c1f41250213c44c1db087478d79b028ef04
jamesome <jamesome@naver.com>
2024-05-22 15:30:47 +0900

    시드용, 마이그레이션용으로 dataSource.ts 분리

--- migration.dataSource.ts
+++ migration.dataSource.ts
@@ -1,6 +1,7 @@
 import { ConfigService } from '@nestjs/config';
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { config as dotenvConfig } from 'dotenv';
+import { SeederOptions } from 'typeorm-extension';
 
 // package.json에 작성 된 typeorm-extension이 appConfigValidationSchema.module을 읽지 못하기 때문에 최상단에 작성 추가.
 // dotenvConfig({ path: `.env.${process.env.NODE_ENV}` });
@@ -8,19 +9,21 @@ dotenvConfig({ path: `.env.development` });
 
 const configService = new ConfigService();
 
-export default new DataSource({
+const options: DataSourceOptions & SeederOptions = {
   type: 'mysql',
   host: configService.getOrThrow<string>('TENANT_DB_HOST'),
   port: configService.getOrThrow<number>('TENANT_DB_PORT'),
-  database: configService.getOrThrow<string>('TENANT_DB_NAME'), // TODO: How..?
-  entities: ['dist/modules/**/**/**/entities/*.entity{.ts,.js}'],
+  database: configService.getOrThrow<string>('TENANT_DB_NAME'),
+  // entities: ['dist/modules/**/**/**/entities/*.entity{.ts,.js}'],
+  entities: ['src/modules/**/**/**/entities/*.entity{.ts,.js}'],
   username: configService.getOrThrow<string>('TENANT_DB_USERNAME'),
   password: configService.getOrThrow<string>('TENANT_DB_PASSWORD'),
   // synchronize: configService.get<string>('NODE_ENV') === 'development', // 서버가 구동될 때, 테이블 자동생성
   synchronize: false,
   logging: configService.get<string>('NODE_ENV') === 'development',
-  // entities: [__dirname + '/src/modules/../../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/src/database/migration/*{.ts,.js}'], // '/../**/migrations/**' => (설정 시) Duplicate migrations 오류(두 번 돌아서 중복 오류)
   // migrations: ['/src/database/migration/*.ts'],
   migrationsTableName: 'migrations',
-});
+};
+
+export const dataSource = new DataSource(options);
---
+++ seed.dataSource.ts
@@ -0,0 +1,33 @@
+import { ConfigService } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config as dotenvConfig } from 'dotenv';
+import { SeederOptions, createDatabase, runSeeders } from 'typeorm-extension';
+
+// package.json에 작성 된 typeorm-extension이 appConfigValidationSchema.module을 읽지 못하기 때문에 최상단에 작성 추가.
+// dotenvConfig({ path: `.env.${process.env.NODE_ENV}` });
+dotenvConfig({ path: `.env.development` });
+
+const configService = new ConfigService();
+
+const options: DataSourceOptions & SeederOptions = {
+  type: 'mysql',
+  host: configService.getOrThrow<string>('TENANT_DB_HOST'),
+  port: configService.getOrThrow<number>('TENANT_DB_PORT'),
+  database: configService.getOrThrow<string>('TENANT_DB_NAME'),
+  entities: ['src/modules/**/**/**/entities/*.entity{.ts,.js}'],
+  username: configService.getOrThrow<string>('TENANT_DB_USERNAME'),
+  password: configService.getOrThrow<string>('TENANT_DB_PASSWORD'),
+  synchronize: false,
+  logging: configService.get<string>('NODE_ENV') === 'development',
+  seeds: ['src/database/seeds/*{.ts,.js}'],
+  seedTracking: true,
+  factories: ['dist/database/factories/*{.ts,.js}'],
+};
+
+createDatabase({ options });
+
+const dataSource = new DataSource(options);
+dataSource.initialize();
+runSeeders(dataSource);
+
+export default dataSource;
