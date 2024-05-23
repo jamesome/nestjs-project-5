2ccfaa1af22ce61c053f57bc73e7a420100d2b9a
jamesome <jamesome@naver.com>
2024-05-23 20:56:19 +0900

    dto 수정

--- src/modules/tenant/warehouse/v1/dto/create-warehouse-v1.dto.ts
+++ src/modules/tenant/warehouse/v1/dto/create-warehouse-v1.dto.ts
@@ -1,6 +1,11 @@
+import { Type } from 'class-transformer';
+import { ValidateNested } from 'class-validator';
 import { CreateProductDto } from 'src/modules/tenant/product/v1/dto/create-product.dto';
 
 export class CreateWarehouseV1Dto {
   name!: string;
+
+  @ValidateNested()
+  @Type(() => CreateProductDto)
   product!: CreateProductDto;
 }
