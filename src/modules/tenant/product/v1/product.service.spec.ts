7c5e8e7a74c8ba158f7378e81440e10a74b4e2c5
jamesome <jamesome@naver.com>
2024-05-23 20:56:48 +0900

    product 서비스 테스트코드

--- src/modules/tenant/product/v1/product.service.spec.ts
+++ src/modules/tenant/product/v1/product.service.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductService } from './product.service';
+import { ProductV1Repository } from './productV1.repository';
+import { CreateProductDto } from './dto/create-product.dto';
 
 describe('ProductService', () => {
   let service: ProductService;
 
   beforeEach(async () => {
+    const mockDataSource = {
+      getRepository: jest.fn().mockReturnValue({
+        create: jest.fn().mockReturnValue({ id: 1, ...new CreateProductDto() }),
+        find: jest.fn(),
+        findOne: jest.fn(),
+        update: jest.fn(),
+        remove: jest.fn(),
+      }),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductService],
+      providers: [
+        ProductService,
+        ProductV1Repository,
+        {
+          provide: 'CONNECTION',
+          useValue: mockDataSource,
+        },
+      ],
     }).compile();
 
     service = module.get<ProductService>(ProductService);
