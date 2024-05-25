63a78ef20ec994344424db3b8082ff55c2266c11
jamesome <jamesome@naver.com>
2024-05-25 22:52:43 +0900

    seed용 DS 수정

--- seed.dataSource.ts
+++ seed.dataSource.ts
@@ -5,23 +5,23 @@ import { SeederOptions, runSeeders } from 'typeorm-extension';
 
 // package.json에 작성 된 typeorm-extension이 appConfigValidationSchema.module을 읽지 못하기 때문에 최상단에 작성 추가.
 // dotenvConfig({ path: `.env.${process.env.NODE_ENV}` });
-dotenvConfig({ path: `.env.development` });
+dotenvConfig();
 
 const configService = new ConfigService();
 
 const options: DataSourceOptions & SeederOptions = {
   type: 'mysql',
-  host: configService.getOrThrow<string>('TENANT_DB_HOST'),
-  port: configService.getOrThrow<number>('TENANT_DB_PORT'),
-  database: configService.getOrThrow<string>('TENANT_DB_NAME'),
-  entities: ['src/modules/**/**/**/entities/*.entity{.ts,.js}'],
-  username: configService.getOrThrow<string>('TENANT_DB_USERNAME'),
-  password: configService.getOrThrow<string>('TENANT_DB_PASSWORD'),
+  host: configService.getOrThrow<string>('DB_HOST'),
+  port: configService.getOrThrow<number>('DB_PORT'),
+  database: configService.getOrThrow<string>('DB_NAME'),
+  entities: [__dirname + '/src/modules/**/**/**/entities/*.entity{.ts,.js}'],
+  username: configService.getOrThrow<string>('DB_USERNAME'),
+  password: configService.getOrThrow<string>('DB_PASSWORD'),
   synchronize: false,
   logging: configService.get<string>('NODE_ENV') === 'development',
-  seeds: ['src/database/seeds/*{.ts,.js}'],
+  seeds: [__dirname + '/src/database/seeds/*{.ts,.js}'],
   seedTracking: false,
-  factories: ['src/database/factories/*{.ts,.js}'],
+  factories: [__dirname + '/src/database/factories/*{.ts,.js}'],
 };
 
 const dataSource = new DataSource(options);
