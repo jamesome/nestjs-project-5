7a499966d917f17951079206452a35ff7b4d2cc5
jamesome <jamesome@naver.com>
2024-05-25 22:42:55 +0900

    마이그에이션 DS 수정

--- migration.dataSource.ts
+++ migration.dataSource.ts
@@ -4,19 +4,19 @@ import { config as dotenvConfig } from 'dotenv';
 
 // package.json에 작성 된 typeorm-extension이 appConfigValidationSchema.module을 읽지 못하기 때문에 최상단에 작성 추가.
 // dotenvConfig({ path: `.env.${process.env.NODE_ENV}` });
-dotenvConfig({ path: `.env.development` });
+dotenvConfig();
 
 const configService = new ConfigService();
 
 const options: DataSourceOptions = {
   type: 'mysql',
-  host: configService.getOrThrow<string>('TENANT_DB_HOST'),
-  port: configService.getOrThrow<number>('TENANT_DB_PORT'),
-  database: configService.getOrThrow<string>('TENANT_DB_NAME'),
-  // entities: ['dist/modules/**/**/**/entities/*.entity{.ts,.js}'],
-  entities: ['src/modules/**/**/**/entities/*.entity{.ts,.js}'],
-  username: configService.getOrThrow<string>('TENANT_DB_USERNAME'),
-  password: configService.getOrThrow<string>('TENANT_DB_PASSWORD'),
+  host: configService.getOrThrow<string>('DB_HOST'),
+  port: configService.getOrThrow<number>('DB_PORT'),
+  database: configService.getOrThrow<string>('DB_NAME'),
+  // entities: [__dirname + 'src/modules/**/**/**/entities/*.entity{.ts,.js}'],
+  entities: [__dirname + '../entities/*.entity{.ts,.js}'],
+  username: configService.getOrThrow<string>('DB_USERNAME'),
+  password: configService.getOrThrow<string>('DB_PASSWORD'),
   // synchronize: configService.get<string>('NODE_ENV') === 'development', // 서버가 구동될 때, 테이블 자동생성
   synchronize: false,
   logging: configService.get<string>('NODE_ENV') === 'development',
