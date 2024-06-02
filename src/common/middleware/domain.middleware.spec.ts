55268866ec951542124785c78b7e80d45060e82d
jamesome <jamesome@naver.com>
2024-06-02 18:43:05 +0900

    🔥 불필요코드 제거

--- src/common/middleware/domain.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DomainMiddleware } from './domain.middleware';
-
-describe('DomainMiddleware', () => {
-  it('should be defined', () => {
-    expect(new DomainMiddleware()).toBeDefined();
-  });
-});
