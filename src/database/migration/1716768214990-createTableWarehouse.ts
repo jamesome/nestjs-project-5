539bfbaff56e7bdda81aec3145266c764d723946
jamesome <jamesome@naver.com>
2024-06-05 08:04:26 +0900

    마이그레이션 변경 및 도메인변경 준비

---
+++ src/database/migration/1716768214990-createTableWarehouse.ts
@@ -0,0 +1,84 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { TimestampedTable } from '../timestamped-table';
+
+export class CreateTableWarehouse1716768214990 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new TimestampedTable({
+        name: 'warehouse',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+            isNullable: false,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '100',
+            isNullable: false,
+            isUnique: true,
+            comment: '창고명',
+          },
+          {
+            name: 'code',
+            type: 'varchar',
+            length: '100',
+            isNullable: true,
+            isUnique: true,
+            comment: '창고코드',
+          },
+          {
+            name: 'post_code',
+            type: 'varchar',
+            length: '10',
+            isNullable: true,
+            comment: '우편번호',
+          },
+          {
+            name: 'address',
+            type: 'varchar',
+            length: '500',
+            isNullable: true,
+            comment: '기본주소',
+          },
+          {
+            name: 'detail_address',
+            type: 'varchar',
+            length: '500',
+            isNullable: true,
+            comment: '상세주소',
+          },
+          {
+            name: 'manager',
+            type: 'varchar',
+            length: '50',
+            isNullable: true,
+            comment: '창고 담당자',
+          },
+          {
+            name: 'contact',
+            type: 'varchar',
+            length: '20',
+            isNullable: true,
+            comment: '연락처',
+          },
+          {
+            name: 'create_worker',
+            type: 'varchar',
+            length: '50',
+            isNullable: false,
+            comment: '창고 등록 작업자',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('warehouse');
+  }
+}
