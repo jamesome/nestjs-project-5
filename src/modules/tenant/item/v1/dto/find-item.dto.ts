3b427294222a5910962848b85b3ba140cf53b47c
jamesome <jamesome@naver.com>
2024-06-10 15:58:12 +0900

    커스텀 데코레이터 추가

--- src/modules/tenant/item/v1/dto/find-item.dto.ts
+++ src/modules/tenant/item/v1/dto/find-item.dto.ts
@@ -1,8 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateItemDto } from './create-item.dto';
-import { Expose } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 import { IsOptional } from 'class-validator';
 import { FindItemLocationDto } from 'src/modules/tenant/item-location/dto/find-item-location.dto';
+import { TransformStringToNumber } from 'src/common/decorators/transform-string-to-number';
 
 export class FindItemDto extends PartialType(CreateItemDto) {
   @IsOptional()
@@ -14,6 +15,8 @@ export class FindItemDto extends PartialType(CreateItemDto) {
   itemLocation!: FindItemLocationDto;
 
   @IsOptional()
+  @Transform(({ value }) => (value === undefined || value === null ? 0 : value))
+  @TransformStringToNumber()
   @Expose({ name: 'include_inventory' })
-  includeInventory!: boolean;
+  includeInventory!: number;
 }
