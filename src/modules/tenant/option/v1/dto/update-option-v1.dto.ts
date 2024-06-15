6fa88c5081b04462fa38366da9eb75481b0275d6
jamesome <jamesome@naver.com>
2024-06-15 21:54:20 +0900

    옵션 삭제

--- src/modules/tenant/option/v1/dto/update-option-v1.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateOptionV1Dto } from './create-option-v1.dto';
-
-export class UpdateOptionV1Dto extends PartialType(CreateOptionV1Dto) {}
--- src/modules/tenant/option/v1/option-v1.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OptionV1Service } from './option-v1.service';
-import { OptionV1Controller } from './option-v1.controller';
-
-@Module({
-  controllers: [OptionV1Controller],
-  providers: [OptionV1Service],
-})
-export class OptionV1Module {}
