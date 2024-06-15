e393065a35c1a14629a5f09f40a037a2b569bf11
jamesome <jamesome@naver.com>
2024-06-15 21:54:37 +0900

    상품 삭제

--- src/modules/tenant/product/v2/dto/update-product-v2.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateProductV2Dto } from './create-product-v2.dto';
-
-export class UpdateProductV2Dto extends PartialType(CreateProductV2Dto) {}
--- src/modules/tenant/product/v2/product-v2.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProductV2Service } from './product-v2.service';
-import { ProductV2Controller } from './product-v2.controller';
-
-@Module({
-  controllers: [ProductV2Controller],
-  providers: [ProductV2Service],
-})
-export class ProductV2Module {}
