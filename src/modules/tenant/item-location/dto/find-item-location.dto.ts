9fbc397d7cfcf08326a409f22028a0dad75027c8
jamesome <jamesome@naver.com>
2024-06-28 08:07:10 +0900

    item-location 삭제

--- src/modules/tenant/item-location/dto/find-item-location.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateItemLocationDto } from './create-item-location.dto';
-
-export class FindItemLocationDto extends PartialType(CreateItemLocationDto) {}
