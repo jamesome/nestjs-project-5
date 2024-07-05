930cf86effa5ba189b9f653e014cc3aa56b32139
jamesome <jamesome@naver.com>
2024-07-05 16:03:10 +0900

    inventory-transactions 삭제

--- src/modules/tenant/inventory-transaction/inventory-transaction.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { InventoryTransactionService } from './inventory-transaction.service';
-import { InventoryTransactionController } from './inventory-transaction.controller';
-
-@Module({
-  controllers: [InventoryTransactionController],
-  providers: [InventoryTransactionService],
-})
-export class InventoryTransactionModule {}
